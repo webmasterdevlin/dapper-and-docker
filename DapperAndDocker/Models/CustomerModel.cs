c5a6edce51d789c38414b8d4924280ba69e01260
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-03-18 17:12:13 +0100

    initial commit

---
+++ DapperAndDocker/Models/CustomerModel.cs
@@ -0,0 +1,11 @@
+namespace DapperAndDocker.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ DapperAndDocker/Models/ItemModel.cs
@@ -0,0 +1,9 @@
+namespace DapperAndDocker.Models
+{
+    public class ItemModel
+    {
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
